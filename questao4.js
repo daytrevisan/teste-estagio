b73c35a36a443195b6a2b615602bf79ae54a1665
Dayane <dayanetm@outlook.com>
2024-03-06 14:20:16 -0300

    questão de lógica

---
+++ questao4.js
@@ -0,0 +1,18 @@
+/*
+Como podemos manusear o interruptor diversas vezes, porém executar apenas duas idas às salas
+Podemos distinguir os interruptores através do calor das lâmpadas
+Para isso, vamos efetuar os seguintes passos:
+
+1. Ligar o primeiro interruptor, aguardar alguns minutos para que a lâmpada se aqueça;
+2. Desligar o primeiro interruptor e ligar o segundo interruptor;
+3. Entrar na sala após ligar o segundo interruptor.
+
+Nessas duas idas, podemos observar que:
+- A lâmpada poderá estar acesa;
+- A lâmpada poderá estar apagada e fria;
+- A lâmpada poderá estar apagada e quente.
+
+- Caso a lâmpada esteja acesa, o interruptor refere-se ao número 2, pois este ficou ligado;
+- Caso a lâmpada esteja apagada e quente, o interruptor refere-se ao número 1, pois esteve ligada por um tempo;
+- Caso a lâmpada esteja apagada e fria, o interruptor refere-se ao número 3, pois este não foi acionado e a lâmpada não foi ligada naquele período.
+*/
